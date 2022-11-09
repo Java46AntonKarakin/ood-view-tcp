8a43041fbb4a5187920bcd9c29925c0555df881e
volnykot <volnykot3@gmail.com>
2022-11-09 20:05:36 +0200

    adds HW#37

--- src/telran/appl/CalculatorAppl.java
+++ src/telran/appl/CalculatorAppl.java
@@ -12,16 +12,11 @@ public class CalculatorAppl {
 
 	public static void main(String[] args) {
 		try {
-			// 1.3.1
-			TcpHandler tcpHandler = new TcpHandler (HOSTNAME, PORT);
-			
-			// 1.3.2
+			TcpHandler tcpHandler = new TcpHandler(HOSTNAME, PORT);
 			NetCalculatorProxy calculatorProxy = new NetCalculatorProxy(tcpHandler);
-			
-			Item [] itemsFromNCP = CalculatorMenu.getCalculatorItems(calculatorProxy);
+			Item[] itemsFromNCP = CalculatorMenu.getCalculatorItems(calculatorProxy);
 			Menu menu = new Menu("Main Menu", itemsFromNCP);
 			menu.perform(new ConsoleInputOutput());
-			tcpHandler.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
