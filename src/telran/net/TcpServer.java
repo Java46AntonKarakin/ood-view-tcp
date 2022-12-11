7746f9fe7734bcfb82556bf486963282dce46a33
volnykot <volnykot3@gmail.com>
2022-12-11 07:56:58 +0200

    cw#46

--- src/telran/net/TcpServer.java
+++ src/telran/net/TcpServer.java
@@ -6,31 +6,49 @@ import java.util.concurrent.Executors;
 import java.io.*;
 
 public class TcpServer implements Runnable {
+	private static final int DEFAULT_N_THREADS = 5;
+	private static final int ACCEPT_TIME_OUT = 100;
 	private ServerSocket serverSocket;
 	private int port;
 	private ApplProtocol protocol;
-	private static int cores = Runtime.getRuntime().availableProcessors();
-	ExecutorService service;
+	private ExecutorService executor;
+	volatile boolean isShutdown = false;
 
-	public TcpServer(int port, ApplProtocol protocol) throws Exception {
+	public TcpServer(int port, ApplProtocol protocol, int nThreads) throws Exception {
 		this.port = port;
 		this.protocol = protocol;
+		executor = Executors.newFixedThreadPool(nThreads);
 		serverSocket = new ServerSocket(port);
-		service = Executors.newFixedThreadPool(cores);
+		serverSocket.setSoTimeout(ACCEPT_TIME_OUT);
+	}
+
+	public TcpServer(int port, ApplProtocol protocol) throws Exception {
+		this(port, protocol, DEFAULT_N_THREADS);
 	}
 
 	@Override
 	public void run() {
 		System.out.println("Server is listening on the port " + port);
-		try {
-			while (true) {
+
+		while (!isShutdown) {
+			try {
 				Socket socket = serverSocket.accept();
-				TcpClientServer clientServer = new TcpClientServer(socket, protocol);
-				service.execute(clientServer);
+				TcpClientServer clientServer = new TcpClientServer(socket, protocol, this);
+				executor.execute(clientServer);
+			} catch (SocketTimeoutException e) {
+				
 			}
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
+			catch (Exception e) {
+				System.out.println(e.getMessage());
+				break;
+			}
 		}
+
+	}
+
+	public void shutdown() {
+		isShutdown = true;
+		executor.shutdown();
 
 	}
 
